8e997686bd246e08a240c2b7977fe79e900b28ef
hola-aryan <aryanisastudent@gmail.com>
2023-11-17 22:29:43 +0530

    JavaScript Basics 1

--- girst_app.js
+++ girst_app.js
@@ -2,4 +2,12 @@ console.log("Hello Js");
 
 const fs = require('fs');
 
+const person = {
+    name : "Aryan",
+    age : 20,
+    greet() {console.log("Hello Ji, from" + this.name);}
+}
+
 fs.writeFileSync('hello.txt','Hello from node JS')
+
+person.greet();
