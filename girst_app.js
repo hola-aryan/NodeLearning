444ee675dd16be693621498fa111f0f691de8bdb
hola-aryan <aryanisastudent@gmail.com>
2023-11-17 22:33:02 +0530

    Assignment 2

--- girst_app.js
+++ girst_app.js
@@ -1,6 +1,16 @@
 console.log("Hello Js");
 
+const { log } = require('console');
 const fs = require('fs');
+
+// Write an arrow function which returns the product of two numbers
+const mult = (a,b) => a*b;
+
+console.log(mult(5,4));
+
+person.greet();
+
+// Watch video 6 as well and create a student object
 
 const person = {
     name : "Aryan",
@@ -9,5 +19,3 @@ const person = {
 }
 
 fs.writeFileSync('hello.txt','Hello from node JS')
-
-person.greet();
