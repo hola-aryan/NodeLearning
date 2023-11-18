c7dc8730811132d633b6c0a0a9be9936fbb08b77
hola-aryan <aryanisastudent@gmail.com>
2023-11-18 13:37:09 +0530

    Routing in node js

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const http = require('http')
 
-const server = http.createServer((res, req)=>{
-    console.log("Aryan Mishra");
-})
+const server = http.createServer((req, res)=>{
+    const url = req.url; 
+    if(url === "/"){
+        res.write('<html>'); 
+        res.write('<head><title>Write Message</title></head>'); 
+        res.write('<body><form action="/message" method="POST"> <input type="text" name="message"> <button type="submit"></button> </form></body>'); 
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === "/home"){
+        res.write('<html>'); 
+        res.write('<head><title>Write Message</title></head>'); 
+        res.write('<body>Welcome to the Home</body>'); 
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === "/about"){
+        res.write('<html>'); 
+        res.write('<head><title>Write Message</title></head>'); 
+        res.write('<body>Welcome to the About</body>'); 
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === "/node"){
+        res.write('<html>'); 
+        res.write('<head><title>Write Message</title></head>'); 
+        res.write('<body>Welcome to the Node Page</body>'); 
+        res.write('</html>');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>'); 
+    res.write('<head><title>Hello World</title></head>'); 
+    res.write('<body>Hello From Node JS Server File name app.js</body>'); 
+    res.write('</html>');
+ })
 
 server.listen(4000)
