7c0b0a1966fb75d7d4fd922690228a29b0ee74cd
hola-aryan <aryanisastudent@gmail.com>
2023-11-19 12:57:54 +0530

    Sending data using post request

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/addProduct',(req,res,next)=>{
+    res.send('<form action="/product" method ="POST"> <input type="text" name="item" placeholder="Add Item"> <input type="number" name="Quantity" placeholder="Quantity"><br> <button type="submit">Submit</button> </form>')
+})
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
     console.log("Hello Running Express");
     res.send('<h1>Hello from Express JS</h1>')
-    next();
 })
+
 
 app.use((req,res,next)=>{
     console.log("Hello Running Express fron 2nd middle Ware");
